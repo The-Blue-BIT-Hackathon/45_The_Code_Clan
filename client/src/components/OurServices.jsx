4d4219b9584a0060125ec6c47994b832caca17f1
Srujanmukund24 <srujanmukund10@gmail.com>
2023-03-12 09:00:28 +0530

    landing page done as per maharastra tourism

--- client/src/components/OurServices.jsx
+++ client/src/components/OurServices.jsx
@@ -1,38 +1,39 @@
 import React from "react";
 import "./style/ourServices.css"
-import img1 from "../images/bag1.jpg"
+import img1 from "../images/bag5.jpg"
+import img2 from "../images/bag3.png"
 
 const OurServices = () => {
   return <>
   {/* <!-- ======= Services Section ======= --> */}
-    <section id="services" class="services section-bg">
-      <div class="container">
+    <section id="services" className="services section-bg">
+      <div className="container">
 
-        <div class="section-title">
+        <div className="section-title">
           <h2 data-aos="fade-in">OUR SERVICES</h2>
           <p data-aos="fade-in"> The Main Movtive Of The Website is to make your trip Well Planed And in your Budget.</p>
         </div>
 
-        <div class="row">
-          <div class="col-md-6 d-flex" data-aos="fade-right">
-            <div class="card">
-              <div class="card-img">
+        <div className="row">
+          <div className="col-md-6 d-flex" data-aos="fade-right">
+            <div className="card">
+              <div className="card-img">
                 <img src={img1} alt="..."/>
               </div>
-              <div class="card-body">
-                <h5 class="card-title"><a href="">Planning</a></h5>
-                <p class="card-text">Selecting The Best place to vist on the bassis of User's prefered parameters </p>
+              <div className="card-body">
+                <h5 className="card-title"><a href="">Planning</a></h5>
+                <p className="card-text">Suggesting a best place and Selecting it  on the bassis of User's prefered parameters </p>
               </div>
             </div>
           </div>
-          <div class="col-md-6 d-flex" data-aos="fade-left">
-            <div class="card">
-              <div class="card-img">
-                <img src={img1} alt="..."/>
+          <div className="col-md-6 d-flex" data-aos="fade-left">
+            <div className="card">
+              <div className="card-img">
+                <img src={img2} alt="..."/>
               </div>
-              <div class="card-body">
-                <h5 class="card-title"><a href="">Booking</a></h5>
-                <p class="card-text">Recommending Sutaiable Lodging and Boarding Options.</p>
+              <div className="card-body">
+                <h5 className="card-title"><a href="">Booking</a></h5>
+                <p className="card-text">Recommending Sutaiable Lodging and Boarding Options.</p>
               </div>
             </div>
 
